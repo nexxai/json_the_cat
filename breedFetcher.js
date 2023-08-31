0003d5b51b1187720cf86d961720762e2b0c2905
JT Smith <nexxai@gmail.com>
2023-08-31 14:06:15 -0600

    Extracted base functionality from breedFetcher to index

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,22 @@
 const request = require("request");
-const query = process.argv[2];
 
-request(
-  `https://dsfasdfapi.thecatapi.com/v1/breeds/search?q=${query}`,
-  (err, response, body) => {
-    if (err) {
-      console.log(`Error: ${err}`);
-      process.exit();
-    }
+const fetchBreedDescription = function(breedName, callback) {
+  request(
+    `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`,
+    (err, response, body) => {
+      if (err) {
+        callback(err);
+        return;
+      }
 
-    const data = JSON.parse(body);
-    if (data.length === 0) {
-      console.log(`Breed not found`);
-    } else {
-      console.log(data[0].description);
+      const data = JSON.parse(body);
+      if (data.length === 0) {
+        callback(`Breed not found`);
+      } else {
+        callback(err, data[0].description);
+      }
     }
-  }
-);
+  );
+};
+
+module.exports = { fetchBreedDescription };
